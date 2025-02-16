3bc9cbb03d06625aa2e47e52a46b0e79db0f6473
any <bluefin27pops@docomo.ne.jp>
2025-02-16 10:54:18 +0900

    first commit

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -12,6 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.test.ui.theme.TestTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +29,53 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Greeting(
+                    //     name = "Android",
+                    //     modifier = Modifier.padding(innerPadding)
+                    // )
+                    TalkCats(modifier=Modifier.padding(innerPadding))
                 }
+
+
             }
         }
     }
 }
 
+// @Composable
+// fun Greeting(name: String, modifier: Modifier = Modifier) {
+//     Text(
+//         text = "Hello $name!",
+//         modifier = modifier
+//     )
+// }
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TalkCats(modifier:Modifier=Modifier){
+    Row(modifier.fillMaxWidth()){
+    Box(modifier
+        .fillMaxWidth(0.5f)
+        .background(color=Color(0x99F9D981),shape=RoundedCornerShape(topStart=8.dp,bottomEnd=8.dp)),
+        contentAlignment = Alignment.Center
+        ){
+            Text("ごはん")
+        }
+    Spacer(modifier.weight(1f))
+    Box(modifier
+        .fillMaxWidth(0.5f)
+        .background(color=Color(0x99F9D981),shape=RoundedCornerShape(topStart=8.dp,bottomEnd=8.dp)),
+        contentAlignment = Alignment.Center
+        ){
+            Text("...。")
+        }
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TestTheme {
-        Greeting("Android")
+        // Greeting("Android")
+        TalkCats()
     }
 }
