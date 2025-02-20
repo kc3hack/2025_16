b595ad4ed29c69656eb7b242fcac9346cf2be82e
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2025-02-20 22:50:38 +0900

    睡眠負債の計算式の追加

--- app/src/main/java/com/example/test/utils/userStatios/SleepManager.kt
+++ app/src/main/java/com/example/test/utils/userStatios/SleepManager.kt
@@ -8,6 +8,7 @@ import com.example.test.data.db.SleepedTimeDatabase
 import com.example.test.data.db.TasksDao
 import com.example.test.data.model.ScheduleType
 import com.example.test.data.model.SleepTimeModel
+import com.example.test.utils.scheduleing.SettingData
 import com.example.test.utils.userStatios.SharedPreferencesHelper
 import java.util.Date
 import java.util.concurrent.TimeUnit
@@ -49,4 +50,30 @@ class SleepManager(context: Context) {
         }
         return sleepTimeSum
     }
+
+    /**
+     * 睡眠負債の基準日数
+     */
+    private val sleepDebtRange = 7L
+
+    /**
+     * 睡眠負債を返す（分単位）
+     */
+    fun getSleepDebt(date: Date): Long {
+        // sleepDebtRange日前の日付を取得
+        val recentDate = Date(date.time - sleepDebtRange * 24 * 60 * 60 * 1000)
+
+        // sleepDebtRange日間の睡眠データを取得
+        val recentSleepedTime = sleepTimeDao.getAllSleepTimes().filter { it.startSleepDate.after(recentDate) }
+
+        // 睡眠時間の合計（ミリ秒）
+        var recentSleepedSum = 0L
+        for (sleepedTime in recentSleepedTime) {
+            recentSleepedSum += sleepedTime.sleepTime
+        }
+
+        // 分単位に変換して返す
+        return (recentSleepedSum / (1000 * 60))-(SettingData().sleepDuration * sleepDebtRange)
+    }
+
 }
